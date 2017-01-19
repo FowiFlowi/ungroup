1868ddbf72cfe4d9730e81c4d3ed28cae567c649
FowiFlowi <fowiflowi@gmail.com>
2017-01-19 02:00:19 +0200

    a

--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,6 +1,10 @@
 module.exports = function(req, res, next) {
 	if (req.url == '/' || req.url == '/home' || req.url.match(/^\/auth/))
 		next();
-	else
-		res.render('errorAuth.jade');
+	else {
+		if (!req.isAuthenticated())
+			res.render('errorAuth.jade');
+		else 
+			next();
+	}
 }
