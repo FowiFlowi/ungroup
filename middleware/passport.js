07c0a3d4946d0c16a5e3075b0fca35ad6de72b3e
FowiFlowi <fowiflowi@gmail.com>
2017-01-21 14:15:50 +0200

    a

--- middleware/passport.js
+++ middleware/passport.js
@@ -14,17 +14,35 @@ module.exports = function (req) {
 	}, (accessToken, refreshToken, profile, done) => {
 
 		logger.info('vk auth: ' + profile.displayName);
-		let userData = {
-			vkId: profile.id,
-			query: req.query,
-			username: profile.displayName,
-			photoUrl: profile.photos[0].value,
-			profileUrl: profile.profileUrl
-		};
 
-		User.findOrCreate(userData, (err, user) => {
-			done(err, user);
-		});
+
+		User.findOne({ vkId: profile.id }, (err, user) => {
+			if (err) {
+				return done(err);
+			}
+
+			if (!user) {
+				let userData = {
+					vkId: profile.id,
+					query: req.query,
+					username: profile.displayName,
+					photoUrl: profile.photos[0].value,
+					profileUrl: profile.profileUrl
+				};
+				user = new User(userData);
+				user.save((err) => {
+					if (err) logger.error(err);
+					return done(err, user);
+				})
+			} else {
+				return done(err, user);
+			}
+		})
+
+
+		// User.findOrCreate(userData, (err, user) => {
+		// 	done(err, user);
+		// });
 
 		// done(null, {								// ===> findOrCreate
 		// 	username: profile.displayName,
@@ -39,7 +57,7 @@ module.exports = function (req) {
 	});
 
 	passport.deserializeUser((user, done) => {
-		User.findById(user.vkId, (err, user) => {
+		User.findOne({ 'vkId': user.vkId }, (err, user) => {
 			done(err, user);
 		})
 		// try {									// ====> findById
--- models/user.js
+++ models/user.js
@@ -16,35 +16,34 @@ let mongoose = require('mongoose'),
 	}),
 	userModel = mongoose.model('User', User);
 
-User.methods.findOrCreate = function(userData, cb) {
-	this.model('User').findOne(userData.vkId, (err, obj) => {
-		if (err) {
-			logger.error(err);
-			cb(err);
+// User.methods.findOrCreate = function(userData, cb) {
+// 	this.model('User').findOne(userData.vkId, (err, obj) => {
+// 		if (err) {
+// 			logger.error(err);
+// 			cb(err);
 
-		} else if (obj) {
-			logger.info('User ' + obj.nickname + ' logged on')
-			cb(null, obj);
+// 		} else if (obj) {
+// 			logger.info('User ' + obj.nickname + ' logged on')
+// 			cb(null, obj);
 
-		} else if (!obj) {
-			let user = new userModel({
-				nickname: 	  userData.query.nickname,
-				groupNumber:  userData.query.groupNumber,
-				vkId: 		  userData.vkId,
-				username: 	  userData.username,
-				photoUrl: 	  userData.photoUrl,
-				profileUrl:   userData.profileUrl
-			});
-			user.save();
-			logger.info('User ' + userData.username + ' has registered');
-			cb(null, user);			
-		}
-	})
-};
+// 		} else if (!obj) {
+// 			let user = new userModel({
+// 				nickname: 	  userData.query.nickname,
+// 				groupNumber:  userData.query.groupNumber,
+// 				vkId: 		  userData.vkId,
+// 				username: 	  userData.username,
+// 				photoUrl: 	  userData.photoUrl,
+// 				profileUrl:   userData.profileUrl
+// 			});
+// 			user.save();
+// 			logger.info('User ' + userData.username + ' has registered');
+// 			cb(null, user);			
+// 		}
+// 	})
+// };
 
-User.methods.findById = function(vkId, cb) {
-	return this.model('User').findOne({ vkId }, cb);
-}
+// User.methods.findById = function(vkId, cb) {
+// 	return this.model('User').findOne({ vkId }, cb);
+// }
 
-// module.exports = userModel;
-module.exports = mongoose.model('User', User);
+module.exports = userModel;
