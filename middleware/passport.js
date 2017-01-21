48be3429b984ce545199c02a5ac0e8723373d70c
FowiFlowi <fowiflowi@gmail.com>
2017-01-21 20:58:38 +0200

    a

--- middleware/passport.js
+++ middleware/passport.js
@@ -31,7 +31,7 @@ module.exports = function (req) {
 						photoUrl: profile.photos[0].value,
 						profileUrl: profile.profileUrl
 					};
-				console.log(query.nickname, '<<THERE!', req);
+				console.log(query.nickname, '<<THERE!', req.query);
 				user = new User(userData);
 				user.save((err) => {
 					if (err) 
