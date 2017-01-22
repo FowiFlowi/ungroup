3f111d75319467a22b3236abc5ff36f7cf9244b7
FowiFlowi <fowiflowi@gmail.com>
2017-01-23 01:02:25 +0200

    a

--- config/index.js
+++ config/index.js
@@ -1,8 +1,7 @@
-let nconf = require('nconf'),
-	path = require('path');
+let nconf = require('nconf');
 
 nconf.argv()
 	.env()
-	.file({file: path.join(__dirname + 'config.json')});
+	.file({file: __dirname + '/config.json'});
 
 module.exports = nconf;
