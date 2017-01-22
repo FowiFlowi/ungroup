60fdcbb012970f277af6875c6a159b2e5af2341f
FowiFlowi <fowiflowi@gmail.com>
2017-01-22 20:20:19 +0200

    return checkAuth

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ module.exports = function (app, server, http) {
 			getScheduleJSON = scheduleModel.getScheduleJSON;
 
 		getScheduleJSON(options, (schedule) => {
-			res.render('schedule', { schedule });
+			res.render('schedule', { page: 'Schedule', schedule });
 		})
 	})
 };
