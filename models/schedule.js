8aefd9235c16b2d8c551f77a6eba9cd5ffbd0971
FowiFlowi <fowiflowi@gmail.com>
2017-01-28 18:44:25 +0200

    a

--- models/schedule.js
+++ models/schedule.js
@@ -23,8 +23,10 @@ module.exports = function(user, query) {
 
 	request(url, (err, res, body) => {
 		if (err) logger.error(err);
+		console.log('1', obj);
 		res.statusCode == 200 ? obj = JSON.parse(body) : logger.info('Fail: ' + res.statusCode);
+		console.log('2', obj);
 	});
-
+	console.log('3', obj);
 	return obj;
 }
